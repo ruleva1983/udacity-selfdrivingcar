beb42868e19768a5f697e6caeb3278da6e8387fb
rvasile <ruggero.vasile@ambrosys.de>
2017-06-14 11:47:22 +0200

    Added script to download data for traffic-sign project

---
+++ term1/traffic_sign/download_data.sh
@@ -0,0 +1,6 @@
+mkdir data
+cd data
+wget "https://d17h27t6h515a5.cloudfront.net/topher/2017/February/5898cd6f_traffic-signs-data/traffic-signs-data.zip"
+unzip traffic-signs-data.zip
+rm -f traffic-signs-data.zip
+cd ..
